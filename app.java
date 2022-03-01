042afbf90a7f869815395fca34849a8fa2122445
Dansereau21 <93531652+Dansereau21@users.noreply.github.com>
2022-03-01 08:39:54 +0100

    Add files via upload

---
+++ app.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+/*
+* File: Oktogon.java
+* Author: Csőke Róbert
+* Copyright: 2022, Csőke Róbert
+* Group: Szoft I_N
+* Date: 2022-02-22
+* Github: https://github.com/Dansereau21
+* Licenc: GNU GPL
+*/
+
+
+
+
+
+public class app {
+	
+	public static void main (String[] args) {
+		System.out.println("Csőke Róbert, 2022.02.22, Szoft_I_N ");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Alap: ");
+		double alap = sc.nextDouble();
+		System.out.println("Szög: ");
+		double szog = sc.nextDouble();
+		
+		double terület = 2 * cot(3,14/8)*a^2;
+		System.out.println("terület:");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
